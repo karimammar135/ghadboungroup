74ea0e00eaeaf247b912ad344f971e319250a2e1
Karim <ka3588828@gmail.com>
2024-07-20 14:44:12 +0300

    remove node_modules before running vercel build

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,10 @@ if [ -d ".git" ]; then
   rm -rf .git
 fi
 
+# Remove the node_modules folder if it exists
+if [ -d "ghadboungroup/node_modules" ]; then
+  rm -rf ghadboungroup/node_modules
+fi
+
 pip3 install -r requirements.txt
 python3.9 manage.py collectstatic
