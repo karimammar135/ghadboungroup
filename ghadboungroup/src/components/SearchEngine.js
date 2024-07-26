b4d57fcd41140d32340ad7a31b4d0551dfacd152
Karim <ka3588828@gmail.com>
2024-07-26 21:06:02 +0300

    add basic search engine

--- ghadboungroup/src/components/SearchEngine.js
+++ ghadboungroup/src/components/SearchEngine.js
@@ -55,10 +55,15 @@ export default function SearchEngine({ setSearchEngine, scrollTo }) {
     return (
         <form onSubmit={() => searchQuery()} ref={ref} className='absolute left-0 w-full pl-[15px] pr-[15px] 820:pl-0 820:pr-0 820:w-fit 820:translate-x-[-90%]'>
             <input onChange={(e) => filterResults(e)} type="text" placeholder="Search something..." className='w-full nos:w-[160px] h-[30px] shadow rounded bg-white mt-[10px] text-center text-[14px]'></input>
-            <div className='w-full nos:w-[160px] shadow bg-white'>
-                {searchResults.map((result, key) => {
-                    return <span className='block w-[100%]' onClick={() => scrollTo((searchOptions[result])[0], (searchOptions[result])[1])} key={key}>{result}</span>
-                })}
+            
+            <div className="w-full nos:w-[160px] container mx-auto p-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {searchResults.map((result, index) => (
+                        <div onClick={() => scrollTo((searchOptions[result])[0], (searchOptions[result])[1])} key={index} className="bg-white border p-4 rounded shadow-sm hover:shadow-md transition-shadow duration-300">
+                            <h5 className="text-xl font-semibold">{result}</h5>
+                        </div>
+                    ))}
+                </div>
             </div>
         </form>
     )
